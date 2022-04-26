1bb7ba51fa16cd2f9c507efde3b6ff2104676e44
Leon Babic <leon@leonbabic.ch>
2022-04-26 10:39:24 +0200

    Add bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import StartOverlay from "./components/StartOverlay/StartOverlay";
 
 function App() {
