0005c5d8eadfc711d0fb80cd06549de4a6d89038
Leon Babic <leon@leonbabic.ch>
2022-04-26 10:21:32 +0200

    Add StartOverlay component

---
+++ src/components/StartOverlay/StartOverlay.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+interface StartOverlayProps {}
+
+const StartOverlay: FC<StartOverlayProps> = () => (
+  <div>StartOverlay Component</div>
+);
+
+export default StartOverlay;
