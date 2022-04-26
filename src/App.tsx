c0c6a1137683cf27ebbabe9dd6baa61cfb47faa2
Leon Babic <leon@leonbabic.ch>
2022-04-26 14:32:04 +0200

    Add calculation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import StartOverlay from "./components/StartOverlay/StartOverlay";
 import Playground from "./components/Playground/Playground";
 
 function App() {
+  const [playground, setPlayground] = useState<String[]>([]);
+
+  const [user, setUser] = useState(true);
+
+  const [won, setWon] = useState(false);
+
+  const addCross = (id: number) => {
+    if (playground[id] === undefined && !won) {
+      let temp = [...playground];
+      temp[id] = user ? "cross" : "circle";
+      setUser(!user);
+      setPlayground(temp);
+      calculateWinner(temp);
+    }
+  };
+
+  const calculateWinner = (playground: String[]) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    lines.forEach((line) => {
+      const [x, y, z] = line;
+      if (
+        playground[x] &&
+        playground[x] === playground[y] &&
+        playground[x] === playground[z]
+      ) {
+        setWon(true);
+      }
+    });
+  };
+
   return (
     <div className="App">
-      <StartOverlay></StartOverlay>
-      <div className="container">
-        <Playground></Playground>
-      </div>
+      <Playground addCross={addCross} playground={playground}></Playground>
+      <h1>
+        Player {user ? "X" : "O"} {}
+      </h1>
     </div>
   );
 }
