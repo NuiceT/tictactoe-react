325d83221d677f6abe650500c1eb26498959a131
Leon Babic <leon@leonbabic.ch>
2022-04-26 10:39:45 +0200

    Add Playground module

---
+++ src/components/Playground/Playground.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+import "./Playground.css";
+
+interface PlaygroundProps {}
+
+const Playground: FC<PlaygroundProps> = () => <div>Playground Component</div>;
+
+export default Playground;
