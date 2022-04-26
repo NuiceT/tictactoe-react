9bec81505bbb77239344daceee203bb2660d5758
Leon Babic <leon@leonbabic.ch>
2022-04-26 11:22:31 +0200

    Add Playground to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import StartOverlay from "./components/StartOverlay/StartOverlay";
+import Playground from "./components/Playground/Playground";
 
 function App() {
   return (
     <div className="App">
       <StartOverlay></StartOverlay>
+      <div className="container">
+        <Playground></Playground>
+      </div>
     </div>
   );
 }
